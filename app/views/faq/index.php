e58431b5593922c9255d93148b15bce2a5d7a3cd
brenatasiva <brenatasiva@gmail.com>
2021-08-02 22:06:47 +0700

    tambah sql

--- app/views/faq/index.php
+++ app/views/faq/index.php
@@ -1,7 +1,14 @@
 <h1 class="mt-4">FAQs</h1>
 
 <div class="accordion" id="accordionExample">
+    <?php
+    $words = array(
+        'units' => array('', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'),
+        'tens' => array('', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety')
+    );
+    ?>
     <?php foreach ($data['listAllFaq'] as $key) : ?>
+        $iterasi = ++;
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingOne">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
