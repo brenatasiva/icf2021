b5ffd1c5572a7f5cbc0e3be3e64275b272339d22
VENANSIUS MARIO TANDO _ <s160419091@student.ubaya.ac.id>
2021-08-04 21:23:21 +0700

    update login

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -10,6 +10,6 @@
     </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary">Login</button>
-        <label for="" class="form-label">Dont have an account yet? <a href="<?= BASEURL; ?>/register">Register</a></label>
+        <label for="" class="form-label">Dont have an account yet? <a href="<?= BASEURL; ?>user/register">Register</a></label>
     </div>
 </form>
